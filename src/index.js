456fae3beef3d3061fc38c9a74a36f6a458f5dd3
Denisio Rodrigues <denisio@ymail.com>
2022-06-25 04:23:46 -0300

    account withdrawal

--- src/index.js
+++ src/index.js
@@ -22,6 +22,18 @@ function verifyIsExistsAccountCPF(request, response, netx){
     netx();
 }
 
+function getBalance(statement) {
+    const balance = statement.reduce((acc, operation) => {
+        if(operation.type === 'credit') {
+            return acc + operation.amount;
+        } else {
+            return acc - operation.amount;
+        }
+    }, 0);
+
+    return balance;
+}
+
 app.post("/account", (request, response) => {
     const {cpf, name } = request.body;
 
@@ -72,5 +84,27 @@ app.post("/deposit", verifyIsExistsAccountCPF, (request, response) => {
 
     return response.status(201).send();
 });
+
+
+app.post("/withdraw", verifyIsExistsAccountCPF, (request, response) => {
+    const { amount } = request.body;
+    const { customer } = request;
+    
+    const balance = getBalance(customer.statments);
+
+    if(amount > balance){
+        return response.status(400).json({error: "Insufucients funds!"});
+    }
+
+    const statmentOperation = {
+        amount,
+        cread_at: new Date(),
+        type: "debit"
+    }
+
+    customer.statments.push(statmentOperation);
+
+    return response.status(201).send();
+})
 
 app.listen(3333);
