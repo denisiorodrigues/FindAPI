2a3e245be8c511ae0ff8c83011963de695dc44f0
Denisio Rodrigues <denisio@ymail.com>
2022-07-10 15:36:59 -0300

    Configuracao debug

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,16 @@ import express from "express"
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (request, response) => {
     return response.json({message: "Hello FindAPI!!!"});
+})
+
+app.post("/accounts", (request, response) => {
+    const { name } = request.body;
+
+    return response.json({"name" : name});
 })
 
 app.listen(3001, () => console.log("Serever is Runnig..."));
